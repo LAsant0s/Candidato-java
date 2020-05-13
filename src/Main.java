3202729a20b6ddee2264dcfb809db10f2031dbd3
Luan Assis <luan.assis05.santos@gmail.com>
2020-05-13 19:31:48 -0300

    Leitura da escolaridade

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//1) leitura da escolaridade
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós Graduação");
+		
+		System.out.print("Digite uma opção: ");
+		int escolaridade = sc.nextInt();
+		
+		
+	}
+
+}
